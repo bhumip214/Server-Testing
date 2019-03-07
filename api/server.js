cc88624f18dc2f05989ca93fb397b86d18d0c63e
bhumip214 <bhumip214@gmail.com>
2019-03-07 15:36:41 -0800

    Delete testing done - MVP completed

--- api/server.js
+++ api/server.js
@@ -34,19 +34,20 @@ server.post("/friends", async (req, res) => {
   }
 });
 
-// router.delete("/friends/:id", async (req, res) => {
-//   try {
-//     const count = await Friends.remove(req.params.id);
-//     if (count > 0) {
-//       res.status(200).json({ message: "The friend has been removed" });
-//     } else {
-//       res.status(404).json({ message: "The friend could not be found" });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({
-//       message: "Error removing the friend"
-//     });
-//   }
-// });
+server.delete("/friends/:id", async (req, res) => {
+  try {
+    const count = await Friends.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: "The friend has been removed" });
+    } else {
+      res.status(404).json({ message: "The friend could not be found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error removing the friend"
+    });
+  }
+});
+
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
-
 const server = require("./server.js");
+const db = require("../data/dbConfig.js");
 
 describe("GET '/'", () => {
   it("should return 200 OK", async () => {
@@ -22,7 +22,7 @@ describe("GET '/'", () => {
   });
 });
 
-describe("friends endpoints", () => {
+describe("Friends endpoints", () => {
   describe("GET '/friends'", () => {
     it("should return 200 OK", async () => {
       const res = await request(server).get("/friends");
@@ -44,6 +44,10 @@ describe("friends endpoints", () => {
   });
 
   describe("POST '/friends'", () => {
+    afterEach(async () => {
+      await db("friends").truncate();
+    });
+
     it("should return 201 when adding a new friend ", async () => {
       const friend = { name: "sarah" };
       const res = await request(server)
@@ -69,6 +73,20 @@ describe("friends endpoints", () => {
         .send(friend);
 
       expect(friend.name).toBe("sarah");
+    });
+  });
+
+  describe("DELETE '/friends/:id'", () => {
+    it("should return JSON", async () => {
+      const res = await request(server).delete("/friends/:id");
+
+      expect(res.type).toEqual("application/json");
+    });
+
+    it("should delete a friend and return 200", async () => {
+      const res = await request(server).delete("/friends/1");
+
+      expect(res.status).toBe(200);
     });
   });
 });
